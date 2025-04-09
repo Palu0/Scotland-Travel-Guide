a7f7301f947fa72a7d6f569fd79cee02520cee2a
Phoenixfeder0 <nicolasfuchs09@gmail.com>
2025-04-09 13:49:43 +0200

    made map less cluttered by hiding stuff in edinburgh and glasgow when zoomed out

--- enf/Map/ZoomThreshold.swift
+++ enf/Map/ZoomThreshold.swift
@@ -1,10 +1,16 @@
 
 func getZoomThreshold(for location:Location) -> Double {
-    switch location.color{
-    case .red: return 0.07
-    case .blue: return 12
-    case .green: return 1
-    case .yellow: return 5
-    default: return 0.1
+    if location.latitude < 56 && location.latitude > 55 {
+        return 0.07
     }
-}
+    else {
+            switch location.color{
+            case .red: return 0.07
+            case .blue: return 12
+            case .green: return 1
+            case .yellow: return 5
+            default: return 0.1
+            }
+        }
+    }
+
